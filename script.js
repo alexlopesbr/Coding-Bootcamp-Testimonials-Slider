6aaf04cd870e67fc2781cc26e011e21c5f674cfc
Alexandre <criabio@hotmail.com>
2020-11-10 20:32:18 -0300

    button bugfix

--- script.js
+++ script.js
@@ -7,6 +7,18 @@ const screenWidth = document.querySelector('.container').clientWidth;
 const arrUsers = Array.prototype.slice.call(users);
 let currentUser = 0;
 
+const quoteTop = document.querySelector('.quote').getBoundingClientRect().top;
+const quoteLeft = document.querySelector('.quote').getBoundingClientRect().left;
+const imgQuote = document.querySelector('.imgQuote');
+
+const buttons = document.querySelector('.buttons');
+const image = document.querySelector('.imgUser');
+const buttonsHeight = buttons.offsetHeight;
+const buttonsWidth = buttons.offsetWidth;
+
+const positionXBtn = image.getBoundingClientRect().left;
+const positionYBtn = image.getBoundingClientRect().bottom;
+
 function addClass(index) {
   users[index].classList.add('active');
 }
@@ -33,30 +45,21 @@ function handleClick(event) {
 btnPrevious.addEventListener('click', handleClick);
 btnNext.addEventListener('click', handleClick);
 
-/* quote position */
-
-const quoteTop = document.querySelector('.quote').getBoundingClientRect().top;
-const quoteLeft = document.querySelector('.quote').getBoundingClientRect().left;
-const imgQuote = document.querySelector('.imgQuote');
-
 /* button position */
 
-const buttons = document.querySelector('.buttons');
-const image = document.querySelector('.imgUser');
-const buttonsHeight = buttons.offsetHeight;
-const buttonsWidth = buttons.offsetWidth;
+function btnAppear() {
+  setTimeout(() => {
+    buttons.style.top = `${positionYBtn - buttonsHeight / 2}px`;
+    if (screenWidth > 1024) {
+      buttons.style.left = `${positionXBtn + 45}px`;
 
-const positionXBtn = image.getBoundingClientRect().left;
-const positionYBtn = image.getBoundingClientRect().bottom;
-
-if (screenWidth > 1024) {
-  buttons.style.left = `${positionXBtn + 45}px`;
-
-  imgQuote.style.top = `${quoteTop - 65}px`;
-  imgQuote.style.left = `${quoteLeft + 125}px`;
-} else {
-  buttons.style.left = `${screenWidth / 2 - buttonsWidth / 2}px`;
-  imgQuote.style.top = `${quoteTop - 23}px`;
+      imgQuote.style.top = `${quoteTop - 65}px`;
+      imgQuote.style.left = `${quoteLeft + 125}px`;
+    } else {
+      buttons.style.left = `${screenWidth / 2 - buttonsWidth / 2}px`;
+      imgQuote.style.top = `${quoteTop - 23}px`;
+    }
+  }, 10);
 }
 
-buttons.style.top = `${positionYBtn - buttonsHeight / 2}px`;
+btnAppear();
